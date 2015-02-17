3cb60633738e88a623cb4dc16517b7899279be57
Evan Yeung <evan.yeung1@gmail.com>
2015-02-16 21:34:51 -0500

    added tiles and changed name

--- src/updateIcon.js
+++ src/updateIcon.js
@@ -2,7 +2,6 @@
 
     // gets the data from the server based on LOCATION
     var getData = function(LOCATION, success, error){
-
         var url = 'http://api.openweathermap.org/data/2.5/weather?id=' + LOCATION.id;
         var r = new XMLHttpRequest();
         r.open('GET', url, true);
@@ -22,13 +21,7 @@
     };
 
     // updates the icon based on server response r
-    var updateIcon = function(r){
-        var data = JSON.parse(r.response);
-
-        // cache for popup
-        data.setAt = Date.now();
-        chrome.storage.local.set({currentWeatherData: JSON.stringify(data)});
-
+    var updateIcon = function(data){
         // update icon itself
         var src;
         var image = data.weather[0].icon.substr(0, 2) + '.png';
@@ -40,18 +33,18 @@
         }
         catch(err){
             src = { // default icon if error
-                19: 'icons/favicons19/01.png',
-                38: 'icons/favicons38/01.png'
+                19: 'icons/favicons19/02.png',
+                38: 'icons/favicons38/02.png'
             };
         }
         chrome.browserAction.setIcon({path: src});
     };
 
     // renders the default icon on error
-    var defaultIcon = function(r){
+    var defaultIcon = function(){
         src = { // default icon if error
-                19: 'icons/favicons19/01.png',
-                38: 'icons/favicons38/01.png'
+                19: 'icons/favicons19/02.png',
+                38: 'icons/favicons38/02.png'
             };
         chrome.browserAction.setIcon({path: src});
     };
@@ -73,9 +66,18 @@
     // initiate the update process
     var initUpdate = function(alarm){
         console.log('updating!');
-        // run getLocation and update the icon
+        // run getLocation and getData
         getLocation(function(LOCATION){
-            getData(LOCATION, updateIcon, defaultIcon);
+            getData(LOCATION, function(r){
+
+                    var data = JSON.parse(r.response);
+                    data.setAt = Date.now();
+                    // cache for popup and fire location changed event
+                    chrome.storage.local.set({currentWeatherData: JSON.stringify(data)});
+
+                },
+                defaultIcon // show default icon if failed
+            );
         });
     };
 
@@ -90,10 +92,18 @@
     chrome.runtime.onStartup.addListener(forStartup);
     // used to track changes in location
     chrome.storage.onChanged.addListener(function(changes, namespace){
-        if(namespace == 'sync' && 'loc' in changes){ // location held in sync
+        if(namespace == 'local' && 'currentWeatherData' in changes){ // location held in sync
             console.log('location changed!');
-            initUpdate();
+
+            // update based on cache
+            if(changes.currentWeatherData.newValue){
+                data = JSON.parse(changes.currentWeatherData.newValue);
+                updateIcon(data);
+                // reschedule alarm
+                chrome.alarms.create('updateIcon', {periodInMinutes: 60});
+            }
         }
     });
 
 })();
+
